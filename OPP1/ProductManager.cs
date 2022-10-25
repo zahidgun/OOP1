b565fff13dcb0505640099a10120813b37316b19
mzahi <mzahi@DESKTOP-6QU8FCL>
2022-10-25 23:41:11 +0300

    Add project files.

---
+++ OPP1/ProductManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+
+            Console.WriteLine(product.ProductName + " Eklendi. ");
+
+        }
+        public void Uptade(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi. ");
+        }
+
+        // Ek işlem Yapabilmek için Return Kullanırız.
+        //public int Topla(int sayi1, int sayi2)
+        //{
+        //    return sayi1 + sayi2;
+        //}
+       
+        //// Emir Kipi, Git Yap Bitir...
+        //public void Topla2(int sayi1, int sayi2)
+        //{
+        //    Console.WriteLine(sayi1+sayi2);
+        //}
+
+    }
+}
---
+++ OPP1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OPP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStok = 3;
+
+
+            Product product2 = new Product { Id = 2, CategoryId = 5, ProductName = "Kalem", UnitPrice = 35, UnitInStok = 5 };
+
+            //PascalCase     //camelCase
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+            productManager.Uptade(product1);
+
+
+            //productManager.Topla2(3,6);
+
+            //int toplamaSonucu = productManager.Topla(3, 6);
+            //Console.WriteLine(toplamaSonucu*2);
+
+          
+
+            //int, double, bool... Değer tip
+            //diziler, class, abstract class, interface... Referans Tip.
+            //REf-OUT
+
+            
+        }
+    }
+}
